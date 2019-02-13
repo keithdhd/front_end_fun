7f073490b54b8d8d31f75bb012b1e45962556a94
cifarquhar <colin.farquhar@codeclan.com>
2019-02-13 13:18:58 +0000

    add getRandomPhrase function

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
-const phrases = [
-  "JavaScript is awesome!",
-  "Should've just used some lorem ipsum...",
-  "How many of these did I write...?",
-  "Some more placeholder text",
-  "Running out of ideas here",
-  "Custard Creams are the best biscuit."
-]
-
-
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM ready");
+
+  const phrases = [
+    "JavaScript is awesome!",
+    "Should've just used some lorem ipsum...",
+    "How many of these did I write...?",
+    "Some more placeholder text",
+    "Running out of ideas here",
+    "Custard Creams are the best biscuit."
+  ]
+
+  function getRandomPhrase() {
+    return phrases[Math.floor(Math.random() * phrases.length)];
+  }
 
   function reqListener () {
     console.log(JSON.parse(this.responseText));
